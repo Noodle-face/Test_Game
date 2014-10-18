d74ef27cdb120546eb9ac02e94f1052859b657a7
Noodle-face <andrewjcorsini@gmail.com>
2014-10-18 15:04:55 -0400

    First commit!

---
+++ CMakeFiles/craft.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Craft/deps/glew/src/glew.c" "C:/Craft/CMakeFiles/craft.dir/deps/glew/src/glew.c.obj"
+  "C:/Craft/deps/lodepng/lodepng.c" "C:/Craft/CMakeFiles/craft.dir/deps/lodepng/lodepng.c.obj"
+  "C:/Craft/deps/noise/noise.c" "C:/Craft/CMakeFiles/craft.dir/deps/noise/noise.c.obj"
+  "C:/Craft/deps/sqlite/sqlite3.c" "C:/Craft/CMakeFiles/craft.dir/deps/sqlite/sqlite3.c.obj"
+  "C:/Craft/deps/tinycthread/tinycthread.c" "C:/Craft/CMakeFiles/craft.dir/deps/tinycthread/tinycthread.c.obj"
+  "C:/Craft/src/auth.c" "C:/Craft/CMakeFiles/craft.dir/src/auth.c.obj"
+  "C:/Craft/src/client.c" "C:/Craft/CMakeFiles/craft.dir/src/client.c.obj"
+  "C:/Craft/src/cube.c" "C:/Craft/CMakeFiles/craft.dir/src/cube.c.obj"
+  "C:/Craft/src/db.c" "C:/Craft/CMakeFiles/craft.dir/src/db.c.obj"
+  "C:/Craft/src/item.c" "C:/Craft/CMakeFiles/craft.dir/src/item.c.obj"
+  "C:/Craft/src/main.c" "C:/Craft/CMakeFiles/craft.dir/src/main.c.obj"
+  "C:/Craft/src/map.c" "C:/Craft/CMakeFiles/craft.dir/src/map.c.obj"
+  "C:/Craft/src/matrix.c" "C:/Craft/CMakeFiles/craft.dir/src/matrix.c.obj"
+  "C:/Craft/src/ring.c" "C:/Craft/CMakeFiles/craft.dir/src/ring.c.obj"
+  "C:/Craft/src/sign.c" "C:/Craft/CMakeFiles/craft.dir/src/sign.c.obj"
+  "C:/Craft/src/util.c" "C:/Craft/CMakeFiles/craft.dir/src/util.c.obj"
+  "C:/Craft/src/world.c" "C:/Craft/CMakeFiles/craft.dir/src/world.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Craft/deps/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "deps/glew/include"
+  "deps/glfw/include"
+  "deps/lodepng"
+  "deps/noise"
+  "deps/sqlite"
+  "deps/tinycthread"
+  "C:/Program Files/cURL/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
